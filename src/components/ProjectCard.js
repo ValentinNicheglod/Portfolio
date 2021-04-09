7d4e50270a8641868d736a97684b364556b90457
Valentín Nicheglod <nicheglod69@gmail.com>
2021-04-09 00:13:40 -0300

    update

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -104,8 +104,11 @@ const ProjectCard = ({name, img, state, github, page, smallScreen, type}) => {
                                 href={github} 
                                 target="_blank" 
                                 rel="noreferrer noopener"
+                                title="Repositorio de GitHub"
                             >
-                                CÓDIGO
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-code-slash" viewBox="0 0 16 16">
+                                    <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z"/>
+                                </svg>
                             </a>
                     }
                     {
@@ -115,10 +118,26 @@ const ProjectCard = ({name, img, state, github, page, smallScreen, type}) => {
                                 href={page} 
                                 target="_blank" 
                                 rel="noreferrer noopener"
+                                title="Visitar web"
                             >
-                                VISITAR
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                                </svg>
+                                
                             </a>
                     }
+                    {/* <a 
+                        className="proyect-btn btn" 
+                        href={page} 
+                        target="_blank" 
+                        rel="noreferrer noopener"
+                        title="Información"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                        </svg>
+                    </a> */}
                 </div>
             </div>
         </Tilt>
