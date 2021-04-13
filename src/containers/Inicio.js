be234ff051a06026ad08eb1fbdb8bf35ccaaedf2
Valentín Nicheglod <nicheglod69@gmail.com>
2021-04-13 02:12:53 -0300

    update

--- src/containers/Inicio.js
+++ src/containers/Inicio.js
@@ -57,17 +57,17 @@ const Inicio = () => {
             >
                 <div className="slide-container h-100">
                     <Slide autoplay={false} className="h-100">
-                        <div className="each-slide h-100">
+                        <div className="each-slide">
                             <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
                             <span>Slide 1</span>
                             </div>
                         </div>
-                        <div className="each-slide h-100">
+                        <div className="each-slide">
                             <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
                             <span>Slide 2</span>
                             </div>
                         </div>
-                        <div className="each-slide h-100">
+                        <div className="each-slide">
                             <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
                             <span>Slide 3</span>
                             </div>
