a9d4ad07fef8ad41a8fdcc14c05032a03c38f94e
Valentín Nicheglod <nicheglod69@gmail.com>
2021-05-23 18:26:11 -0300

    galeria_mobile

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ProjectCard from './ProjectCard';
 
 const projects = [
@@ -7,6 +7,7 @@ const projects = [
         img: 'mono.png',
         state: 'Finalizado',
         gallery: ['mono/inicio',  'mono/catalogo', 'mono/catalogo-filtro'],
+        mobile_gallery: ['mono/mobile/17', 'mono/mobile/18', 'mono/mobile/19'],
         github: 'https://github.com/ValentinNicheglod/MONO',
         page: 'https://valentinnicheglod.github.io/MONO-/',
         type: 'Web'
@@ -15,7 +16,8 @@ const projects = [
         name: 'NotatKy',
         img: 'notatKy.png',
         state: 'Finalizado',
-        gallery: ['notatky/inicio', 'notatky/login', 'notatky/signup', 'notatky/contra1', 'notatky/contra2', 'notatky/contra3', 'notatky/home', 'notatky/home-editing', 'notatky/home-editing-2', 'notatky/home-editing-3', 'notatky/home-add-tag', 'notatky/profile', 'notatky/edit-data', 'notatky/edit-pw', 'notatky/col-tag',],
+        gallery: ['notatky/inicio', 'notatky/login', 'notatky/signup', 'notatky/contra1', 'notatky/contra2', 'notatky/contra3', 'notatky/home', 'notatky/home-editing', 'notatky/home-editing-2', 'notatky/home-editing-3', 'notatky/home-add-tag', 'notatky/profile', 'notatky/edit-data', 'notatky/edit-pw', 'notatky/col-tag'],
+        mobile_gallery: ['notatky/mobile/1', 'notatky/mobile/2', 'notatky/mobile/3', 'notatky/mobile/4', 'notatky/mobile/5', 'notatky/mobile/14', 'notatky/mobile/15', 'notatky/mobile/16', 'notatky/mobile/6', 'notatky/mobile/7', 'notatky/mobile/8', 'notatky/mobile/9', 'notatky/mobile/10', 'notatky/mobile/11', 'notatky/mobile/12', 'notatky/mobile/13'],
         github: 'https://github.com/ValentinNicheglod/notatky-frontend',
         page: 'https://valentinnicheglod.github.io/NotatKy/#/',
         type: 'Web'
@@ -33,6 +35,8 @@ const projects = [
 
 const Proyects = ({smallScreen}) => {
 
+    const [deviceType, setDeviceType] = useState('pc')
+
     return (
         <div className="bg-2 row min m-0">
             <div className="p-4">
@@ -46,12 +50,15 @@ const Proyects = ({smallScreen}) => {
                         projects.map((project, index) => (
                             <>
                             <ProjectCard
+                                deviceType={deviceType}
                                 github= {project.github}
                                 img= {project.img}
                                 key={index}
                                 gallery={project.gallery}
+                                mobile_gallery={project.mobile_gallery}
                                 name= {project.name}
                                 page= {project.page}
+                                setDeviceType={setDeviceType}
                                 smallScreen={smallScreen}
                                 state= {project.state}
                                 type= {project.type}
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,7 +6,7 @@ import {
   CarouselIndicators,
 } from 'reactstrap';
 
-const Slider = ({images, logo, name, page}) => {
+const Slider = ({images, logo, mobile, name, page, setDeviceType}) => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
 
@@ -85,14 +85,16 @@ const Slider = ({images, logo, name, page}) => {
           </div>
         </div>
     </div>
-    {/* <div className="gallery-selector">
-      <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
-        <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked/>
-        <label className="btn btn-outline-dark" for="btnradio1">Computadora</label>
-        <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autocomplete="off"/>
-        <label className="btn btn-outline-dark" for="btnradio3">Smartphone</label>
+    {mobile &&
+      <div className="gallery-selector">
+        <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+          <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked onClick={() => setDeviceType("computer")}/>
+          <label className="btn btn-outline-dark" for="btnradio1">Computadora</label>
+          <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autocomplete="off" onClick={() => setDeviceType("phone")}/>
+          <label className="btn btn-outline-dark" for="btnradio3">Smartphone</label>
+        </div>
       </div>
-    </div> */}
+    }
     </>
   );
 }
