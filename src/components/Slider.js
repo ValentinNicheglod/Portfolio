8f8150bcc677569080154d019057268f4fb39106
Valentín Nicheglod <nicheglod69@gmail.com>
2021-05-23 20:25:52 -0300

    slider-indicators

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -93,7 +93,7 @@ const Slider = ({deviceType, images, logo, mobile, name, page, setDeviceType}) =
         <div className="slider-indicators">
           {
             images && images.map((image, index) => (
-              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill={activeIndex === index ? '#212529' : 'gray'} class="bi bi-circle-fill" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill={activeIndex === index ? '#212529' : 'gray'} class="bi bi-circle-fill" viewBox="0 0 16 16" onClick={() => goToIndex(index)}>
                 <circle cx="8" cy="8" r="8"/>
               </svg>
             ))
