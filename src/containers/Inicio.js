fc52051bcea88b883233f3268e57aa8a0238dfd8
Valentín Nicheglod <nicheglod69@gmail.com>
2021-06-17 21:04:33 -0300

    preload-images

--- src/containers/Inicio.js
+++ src/containers/Inicio.js
@@ -16,6 +16,22 @@ const Inicio = () => {
     }
   }, false);
 
+  const images = [];
+  function preload() {
+    setTimeout(() => {
+      for (let i = 0; i < preload.arguments.length; i += 1) {
+        images[i] = new Image();
+        images[i].src = preload.arguments[i];
+      }
+    }, 3000);
+  }
+
+  preload(
+    'https://img.icons8.com/fluent/96/000000/iphone.png',
+    'https://img.icons8.com/fluent/96/000000/monitor.png',
+    'https://img.icons8.com/fluent/96/000000/checkmark.png',
+  );
+
   const smallScreen = window.screen.width < 600;
 
   return (
